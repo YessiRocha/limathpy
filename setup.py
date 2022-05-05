034d1d29cb742818e20bd5b8ebf5f2841c0b66cb
KarenLechuga <98918000+KarenLechuga@users.noreply.github.com>
2022-05-05 16:20:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='pylima',
+setup(name='limathpy',
       version='0.2',
       description='A Python package for undergraduate math',
       url='http://github.com/YessiRocha/pylima',
       author='Yessica Rocha',
       author_email='yesi.bundo@gmail.com',
       license='MIT',
-      packages=['pylima'],
+      packages=['limathpy'],
       install_requires=['sympy', 'numpy', 'matplotlib'],
       zip_safe=False)

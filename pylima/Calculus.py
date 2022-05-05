faa4bf17fa0fcddba64dc87ee85b9d35d56345f8
YessiRocha <47038042+YessiRocha@users.noreply.github.com>
2022-05-05 12:55:31 -0500

    Update Calculus.py
    Se añaden las funciones n_derivates y graph_fyd

--- pylima/Calculus.py
+++ pylima/Calculus.py
@@ -1,6 +1,36 @@
 #my file of calculus in one variable
-def saludo(palabra):
-  return palabra+" ¿cómo estás?"
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt 
 
 
-
+x = sp.symbols('x')
+def n_derivates(expr, n=1):
+    """Function that returns a list with the n derivates of an expression, with n given"""
+    derivates = [expr]
+    for i in range(n):
+        derivates.append(sp.Derivative(derivates[-1], x).doit())
+    return derivates
+  
+  
+  def graph_fyd(expresion):
+    """Function that graphs an expression given as a string and its derivative in the same plane"""
+    x = sp.symbols('x')
+    expr = sp.sympify(expresion)
+    deriv = sp.diff(expr, x)
+    f, f_prime = sp.lambdify(x, expr, 'numpy'), sp.lambdify(x, deriv, 'numpy')
+    domain = np.linspace(-10, 10)
+    f_eval = f(domain)
+    f_prime_eval = f_prime(domain)
+    if type(f_prime_eval) == float or type(f_prime_eval) == int:
+        sino = "jala"
+        for j in range(len(domain)-1):
+            f_prime_eval=np.append(f_prime_eval, [f_prime(domain)])
+            
+    
+    fig, ax = plt.subplots()
+    ax.set_title("Función y derivada")
+    ax.plot(domain, f_eval)
+    ax.plot(domain, f_prime_eval)
+    ax.set_xlabel("$x$")
+    plt.show()

03271bcf14bfa7101a3974efb63913a2c09cc298
VeraniaHdez12 <47038209+VeraniaHdez12@users.noreply.github.com>
2022-05-05 17:19:56 -0500

    Update Calculus.py

--- limathpy/Calculus.py
+++ limathpy/Calculus.py
@@ -18,27 +18,27 @@ def n_derivates(expr, n=1):
     return derivates
   
   
-  def graph_fyd(expresion):
-      """Function that graphs an expression given as a string and its derivative in the same plane
-      Args:
-          param1 = expression of a function as a string
-      Returns:
-          The graph of the function and its derivative in the same plane """
-      x = sp.symbols('x')
-      expr = sp.sympify(expresion)
-      deriv = sp.diff(expr, x)
-      f, f_prime = sp.lambdify(x, expr, 'numpy'), sp.lambdify(x, deriv, 'numpy')
-      domain = np.linspace(-10, 10)
-      f_eval = f(domain)
-      f_prime_eval = f_prime(domain)
-      if type(f_prime_eval) == float or type(f_prime_eval) == int:
-         for j  in range(len(domain)-1):
-             f_prime_eval=np.append(f_prime_eval, [f_prime(domain)])
+def graph_fyd(expresion):
+    """Function that graphs an expression given as a string and its derivative in the same plane
+    Args:
+        param1 = expression of a function as a string
+    Returns:
+        The graph of the function and its derivative in the same plane """
+    x = sp.symbols('x')
+    expr = sp.sympify(expresion)
+    deriv = sp.diff(expr, x)
+    f, f_prime = sp.lambdify(x, expr, 'numpy'), sp.lambdify(x, deriv, 'numpy')
+    domain = np.linspace(-10, 10)
+    f_eval = f(domain)
+    f_prime_eval = f_prime(domain)
+    if type(f_prime_eval) == float or type(f_prime_eval) == int:
+       for j  in range(len(domain)-1):
+           f_prime_eval=np.append(f_prime_eval, [f_prime(domain)])
             
     
-      fig, ax = plt.subplots()
-      ax.set_title("Función y derivada")
-      ax.plot(domain, f_eval)
-      ax.plot(domain, f_prime_eval)
-      ax.set_xlabel("$x$")
-      plt.show()
+    fig, ax = plt.subplots()
+    ax.set_title("Función y derivada")
+    ax.plot(domain, f_eval)
+    ax.plot(domain, f_prime_eval)
+    ax.set_xlabel("$x$")
+    plt.show()

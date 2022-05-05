f974d36446ed8c78d230d747a3b8f9984769719f
YessiRocha <47038042+YessiRocha@users.noreply.github.com>
2022-05-05 13:13:13 -0500

    Cambios en la documentación de las funciones

--- pylima/Calculus.py
+++ pylima/Calculus.py
@@ -6,7 +6,12 @@ import matplotlib.pyplot as plt
 
 x = sp.symbols('x')
 def n_derivates(expr, n=1):
-    """Function that returns a list with the n derivates of an expression, with n given"""
+    """Function that returns a list with the n derivates of an expression, with n given
+    Args: 
+        param1 = a sympy expression of a function
+        param2 = the number of derivatives required (this parameter is optional, by default returns one derivative)
+    Returns: 
+        A list with the funtion and the indicated derivatives"""
     derivates = [expr]
     for i in range(n):
         derivates.append(sp.Derivative(derivates[-1], x).doit())
@@ -14,7 +19,11 @@ def n_derivates(expr, n=1):
   
   
   def graph_fyd(expresion):
-    """Function that graphs an expression given as a string and its derivative in the same plane"""
+    """Function that graphs an expression given as a string and its derivative in the same plane
+    Args:
+        param1 = expression of a function as a string
+    Returns:
+        The graph of the function and its derivative in the same plane """
     x = sp.symbols('x')
     expr = sp.sympify(expresion)
     deriv = sp.diff(expr, x)
@@ -23,7 +32,6 @@ def n_derivates(expr, n=1):
     f_eval = f(domain)
     f_prime_eval = f_prime(domain)
     if type(f_prime_eval) == float or type(f_prime_eval) == int:
-        sino = "jala"
         for j in range(len(domain)-1):
             f_prime_eval=np.append(f_prime_eval, [f_prime(domain)])
             

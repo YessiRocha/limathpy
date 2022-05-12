1471be84c5da732d56542bd88af1519815c43e81
RosaGranados99 <98921544+RosaGranados99@users.noreply.github.com>
2022-05-12 16:13:26 -0500

    Update __init__.py

--- limathpy/__init__.py
+++ limathpy/__init__.py
@@ -1,5 +1,5 @@
 from limathpy.ODE import *
-from limathpy.NumericalAnalysis import *    
+from limathpy.Mathematical_models import *    
 from limathpy.Calculus import *
 from limathpy.LinearAlgebra import *
 from limathpy.Statistics import *

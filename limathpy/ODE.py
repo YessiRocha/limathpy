1ea56baf5d3ca8c4b6334d0abce4fd6e4993e80c
KarenLechuga <98918000+KarenLechuga@users.noreply.github.com>
2022-05-17 21:44:05 -0500

    Update ODE.py

--- limathpy/ODE.py
+++ limathpy/ODE.py
@@ -5,51 +5,105 @@ x = Function('x')
 C1 = symbols('C1')
 C2 = symbols('C2')
 
-#Resolución de ecuaciones diferenciales ordinarias.
+#Solving ordinary differential equations.
 
-def first_ode(vector): #vector=(función que acompaña a la y'(t), función que acompaña a la y(t), función que depende de t)
-    """Dado un vector, regresa solución general de una ecuación diferencial lineal ordinaria de primer orden."""
+
+#Solving first order ordinary differential equations of the form p(t)y'(t) + q(t)y(t) = g(t).
+
+def first_ode(vector): 
+    """A function that returns the general solution of a first order differential equation of the form p(t)y'(t) + q(t)y(t) = g(t);
+       p, q and g are functions which depend on t.
+    Args: 
+        vector (list): a list of the form [p(t), q(t), g(t)].
+    Returns:
+        Eq: the general solution of the equation, C1 is a constant which depends on some initial condition.
+    Example: 
+        >>> from limathpy import first_ode
+        >>> first_ode([t, 2, 2 + t])
+        Eq(y(t), C1/t**2 + t/3 + 1)"""
     eq = Eq(vector[0]*Derivative(y(t), t) + vector[1]*y(t), vector[2])
     sol = dsolve(eq, y(t))
     return sol
 
 
-def solve_first_ode(vector, cond_inic=[1,0]): #cond_inic es y(t=1)=0 ; vector=(función que acompaña a la y'(t), función que acompaña a la y(t), función que depende de t)
+def solve_first_ode(vector, init_cond = [1, 0]): 
+    """A function that returns the solution of a first order differential equation of the form p(t)y'(t) + q(t)y(t) = g(t) 
+       for some given initial conditions; p, q and g are functions which depend on t.
+    Args: 
+        vector (list): a list of the form [p(t), q(t), g(t)].
+        init_cond (list): a list of the form [t1, y(t1)], for some t1. Defaults to y(t1 = 1) = 0.
+    Returns:
+        Eq: the solution of the equation depending on the initial condition.
+    Example: 
+        >>> from limathpy import solve_first_ode
+        >>> solve_first_ode([t, 2, 2 + t], [1, 1])
+        Eq(y(t), t/3 + 1 - 4/(3*t**2))"""
     equation = first_ode(vector).rhs
-    evalu = equation.subs({t: cond_inic[0]})
+    evalu = equation.subs({t: init_cond[0]})
     lin = Eq(evalu, 0)
     solu = solve(lin, C1)
     final = equation.subs({C1: solu[0]})
     return Eq(y(t), final)
 
-#Para resolver EDO de segundo orden de la forma y'' + p(t)y' + q(t)y + g(t)=0.
 
-def second_ode_const(vector): #vector=(función que acompaña a la y''(t),función que acompaña a la y'(t), función que acompaña a la y(t), función que depende de t)
-    """Dado un vector, resuelve una ecuación diferencial ordinaria de segundo orden con constantes C1 y C2."""
+#Solving first order ordinary differential equations of the form r(t)y''(t) + p(t)y'(t) + q(t)y(t) = g(t).
+
+def second_ode_const(vector): 
+    """A function that returns the solution of a second order differential equation of the form r(t)y''(t) + p(t)y'(t) + q(t)y(t) = g(t); 
+       r, p, q and g are functions which depend on t.
+    Args: 
+        vector (list): a list of the form [r(t), p(t), q(t), g(t)].
+    Returns:
+        Eq: the general solution of the equation, C1 and C2 are constants which depend on some initial conditions.
+    Example: 
+        >>> from limathpy import second_ode_const
+        >>> second_ode_const([t**2, 2*t, 0, 1])
+        Eq(y(t), C1 + C2/t + log(t))"""
     eq = Eq(vector[0]*Derivative(y(t), t, 2) + vector[1]*Derivative(y(t), t) + vector[2]*y(t), vector[3])
     sol = dsolve(eq, y(t))
     return sol
 
 
-def solve_2nd_edo(vector, cond_inic=[[1, 0], [0, 1]]): #y(1)=0, y(0)=1 vector=(y'', y', y, g(t)
-    """Dado un vector y condiciones iniciales resuelve una ecuación diferencial ordinaria de segundo orden con constantes."""
+def solve_2nd_edo(vector, init_cond = [[1, 2], [0, 1]]): #y(1)=0, y(0)=1 vector=(y'', y', y, g(t)
+    """A function that returns the solution of a second order differential equation of the form r(t)y''(t) + p(t)y'(t) + q(t)y(t) = g(t) 
+       for some given initial conditions; r, p, q and g are functions which depend on t.
+    Args: 
+        vector (list): a list of the form [r(t), p(t), q(t), g(t)].
+        init_cond (list of two lists): a matrix of the form [[t1, t2], [y(t1), y(t2)]], for some t1, t2. Defaults to y(t1 = 1) = 0 and y(t2 = 2) = 1.
+    Returns:
+        Eq: the general solution of the equation, C1 and C2 are constants which depend on some initial conditions.
+    Example: 
+        >>> from limathpy import solve_2nd_edo
+        >>> solve_2nd_edo([t**2, 2*t, 0, 1], cond_inic=[[1, 0], [2, 0]])
+        Eq(y(t), log(t) - 2*log(2) + 2*log(2)/t)"""
     equation = second_ode_const(vector).rhs
-    evalu1 = equation.subs({t: cond_inic[0][0]})
-    evalu2 = equation.subs({t: cond_inic[1][0]})
-    eq1 = Eq(evalu1, cond_inic[0][1])
-    eq2 = Eq(evalu2, cond_inic[1][1])
+    evalu1 = equation.subs({t: init_cond[0][0]})
+    evalu2 = equation.subs({t: init_cond[1][0]})
+    eq1 = Eq(evalu1, init_cond[0][1])
+    eq2 = Eq(evalu2, init_cond[1][1])
     const = solve((eq1, eq2))
     final = equation.subs(const)
     return Eq(y(t), final)
 
-#Para resolver sistemas de ecuaciones diferenciales ordinarias.
 
-def sistema(matriz):
-    """Dada un lista de listas, regresa un sistema de ecuaciones diferenciales."""
+#Solving ordinary differential equations systems.
+
+def system_ode(matrix):
+    """A function that, given a 2x2 matrix (list of two lists), returns the general sotutions of the associated ordinary differential equations system.
+    Args: 
+        matrix (list of two lists): a list of two lists of the form [[t1, t2], [t3, t3]], 
+                                    where you obtain the following system x'(t) = t1*x(t) + t2*y(t); y'(t) = t3*x(t) + t4*y(t). 
+    Returns:
+        tuple: a tuple of the form (x(t), y(t)), with x(t) and y(t) the general solutions of the system. C1 and C2 are constants that depend on some initial conditions.
+    Example:
+    >>> from limathpy import system_ode
+    >>> system_ode([[1, 0], [0, -3]])
+    (C1*exp(t), C2*exp(-3*t))
+    """
     t = symbols('t')
     x, y = symbols('x y', cls=Function)
-    eq1 = Eq(Derivative(x(t), t), matriz[0][0]*x(t) + matriz[0][1]*y(t))
-    eq2 = Eq(Derivative(y(t), t), matriz[1][0]*x(t) + matriz[1][1]*y(t))
+    eq1 = Eq(Derivative(x(t), t), matrix[0][0]*x(t) + matrix[0][1]*y(t))
+    eq2 = Eq(Derivative(y(t), t), matrix[1][0]*x(t) + matrix[1][1]*y(t))
     sols = dsolve((eq1, eq2))
     return sols[0].rhs, sols[1].rhs
 
@@ -58,7 +112,7 @@ def sistema_lineal(matriz, cond_inic=[[1, 2], [3, 4]]): #x(1)=3, y(2)=4 vector=(
     """Dado un sistema de ecuaciones diferenciales, regresa el sistema lineal en t=0."""
     t = symbols('t')
     x, y = symbols('x y', cls=Function)
-    sols = sistema(matriz)
+    sols = system_ode(matriz)
     lineal1 = Eq(sols[0].subs({t:cond_inic[0][0]}), cond_inic[1][0])
     lineal2 = Eq(sols[1].subs({t:cond_inic[0][1]}), cond_inic[1][1])
     return lineal1, lineal2
@@ -69,7 +123,7 @@ def sistema_ed(matriz, cond_inic):
     t = symbols('t')
     x, y = symbols('x y', cls=Function)
     C1, C2 = symbols('C1 C2')
-    sis_ed = sistema(matriz)
+    sis_ed = system_ode(matriz)
     sis_lin = sistema_lineal(matriz, cond_inic)
     dict_sols = solve(sis_lin)
     expr1 = sis_ed[0].subs(dict_sols)
@@ -79,7 +133,7 @@ def sistema_ed(matriz, cond_inic):
 #Retrato de fase.
 
 def phase_portrait(matriz, lim_initialconditions=2):
-    system = sistema(matriz)
+    system = system_ode(matriz)
     p = plot_parametric((0,0), (t, 0, 0), show=False, title = 'Phase portrait')
     for i in range(0, lim_initialconditions):
         for j in range(0, lim_initialconditions):

7fb4a24ef43e3af5f8a19009dd8796a6cf571598
juandiego2104 <42421698+juandiego2104@users.noreply.github.com>
2022-05-23 17:55:59 -0500

    Update __init__.py

--- limathpy/__init__.py
+++ limathpy/__init__.py
@@ -2,4 +2,4 @@ from limathpy.ODE import *
 from limathpy.MathematicalModels import *
 from limathpy.Calculus import *
 from limathpy.LinearAlgebra import *
-from limathpy.Statistics import *
+from limathpy.CalculusOfSeveralVariables import *

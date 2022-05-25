513814747905c70eba7b645f522debf2525474d4
juandiego2104 <42421698+juandiego2104@users.noreply.github.com>
2022-05-25 17:32:08 -0500

    Hessian Matrix Function
    Function that returns the Hessian matrix of the given coordinate function.

--- limathpy/CalculusOfSeveralVariables.py
+++ limathpy/CalculusOfSeveralVariables.py
@@ -87,3 +87,38 @@ def jacobian(function , var):
         for j in range(n):
             mat[i, j] = sp.diff(function[i], var[j])
     return mat
+
+
+def hessian(funcion, var):
+    """Function that returns a matrix that represents the Hessian
+       matrix of the given coordinate functions.
+       
+    Args:
+        function: Any list of sympy functions.
+        var: List of variables in order.
+        
+    Returns:
+        list: with the reverse of the function.
+        
+    Example:
+        >>> from sympy import symbols
+        >>> x = symbols('x')
+        >>> y = symbols('y')
+        >>> z = symbols('z')
+        >>> hessian([x*y*z, x*sp.cos(y) -z, x*y*sp.sin(z)], [x, y, z])
+        Matrix([
+        [0, 0, 0],
+        [0, -x*cos(y), 0],
+        [0, 0, -x*y*sin(z)]])
+    """
+    x = sp.symbols('x')
+    y = sp.symbols('y')
+    z = sp.symbols('z')
+    m = len(funcion)
+    n = len(var)
+    mat = sp.zeros(m, n)
+    for i in range(m):
+        for j in range(n):
+            jacb = sp.diff(funcion[i], var[j])
+            mat[i, j] = sp.diff(jacb, var[j])
+    return mat

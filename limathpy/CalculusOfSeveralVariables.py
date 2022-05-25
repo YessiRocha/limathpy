b25e5cead091e9a68d807cfc9755ffcb6525671a
juandiego2104 <42421698+juandiego2104@users.noreply.github.com>
2022-05-25 12:26:45 -0500

    Gradient Function
    This function returns the gradient of a function of several variables

--- limathpy/CalculusOfSeveralVariables.py
+++ limathpy/CalculusOfSeveralVariables.py
@@ -25,3 +25,31 @@ def partial_derivate(expr, var):
     z = sp.symbols('z')
     partial = sp.diff(expr, var)
     return partial
+
+
+def gradient(expr, var):
+    """Function that returns a list representing the function and
+       the gradient of the given function.
+       
+    Args:
+        expr: Any sympy function.
+        var: List of variables in order.
+        
+    Returns:
+        list: with the reverse of the function.
+        
+    Example:
+        >>> from sympy import symbols
+        >>> x = symbols('x')
+        >>> y = symbols('y')
+        >>> z = symbols('z')
+        >>> partial_derivate(x*sp.cos(3*z)**2 + sp.sin(y), [x, y, z])
+        [x*cos(3*z)**2 + sin(y), cos(3*z)**2, cos(y),
+        -6*x*sin(3*z)*cos(3*z)]"""
+    x = sp.symbols('x')
+    y = sp.symbols('y')
+    z = sp.symbols('z')
+    gradient = [expr]
+    for i in range(len(var)):
+        gradient.append(sp.diff(gradient [0], var[i]))
+    return gradient
